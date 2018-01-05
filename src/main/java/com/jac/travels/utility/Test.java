7059eaf072265522bd88e3c914bd918b5321a875
dbshspaul <dbshspaul@gmail.com>
2018-01-05 08:04:47 +0530

    create protobuff response

--- src/main/java/com/jac/travels/utility/Test.java
+++ src/main/java/com/jac/travels/utility/Test.java
@@ -1,23 +1,42 @@
 package com.jac.travels.utility;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jac.travels.model.Property;
 import com.jac.travels.model.Rate;
+import com.jac.travels.model.RatePlan;
+import com.jac.travels.model.Room;
 import org.springframework.data.cassandra.mapping.PrimaryKey;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * created by My System on 15-Dec-17
  **/
 public class Test {
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        PrimaryKey annotation = Rate.class.getDeclaredAnnotation(PrimaryKey.class);
-        for (Field field : Rate.class.getDeclaredFields()) {
-            if(field.isAnnotationPresent(PrimaryKey.class))
-            System.out.println(field.getName()+" = " + field.getAnnotation(PrimaryKey.class).value());
+    public static void main(String[] args) {
+        String str = "";
+        Field[] declaredFields = Room.class.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            String fieldName = declaredField.getName();
+            PrimaryKey declaredAnnotation = declaredField.getAnnotation(PrimaryKey.class);
+            if (declaredAnnotation != null) {
+                Field[] idFields = declaredField.getType().getDeclaredFields();
+                for (Field idField : idFields) {
+                    String idFieldName = idField.getName();
+                    str +=  getString(idFieldName)
+                            +"(room.get"+fieldName.replaceFirst(fieldName.substring(0, 1), fieldName.substring(0, 1).toUpperCase())+"().get"+idFieldName.replaceFirst(idFieldName.substring(0, 1), idFieldName.substring(0, 1).toUpperCase())+ "())\n";
+                }
+            } else {
+                str += getString(fieldName)
+                        +"(room.get"+fieldName.replaceFirst(fieldName.substring(0, 1), fieldName.substring(0, 1).toUpperCase())+ "())\n";
+            }
         }
+        System.out.println(str);
+    }
+
+    private static String getString(String idFieldName) {
+        String str = ".set";
+        for (String s : idFieldName.split("_")) {
+            str += s.replaceFirst(s.substring(0, 1), s.substring(0, 1).toUpperCase());
+        }
+        return str;
     }
 }
