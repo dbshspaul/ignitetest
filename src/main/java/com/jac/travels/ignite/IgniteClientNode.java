c839e65980e2202ee7582d63434a9f1540e8e998
dbshspaul <dbshspaul@gmail.com>
2018-02-12 22:30:25 +0530

    swagger hub

--- src/main/java/com/jac/travels/ignite/IgniteClientNode.java
+++ src/main/java/com/jac/travels/ignite/IgniteClientNode.java
@@ -8,8 +8,14 @@ import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.DataStorageConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.cache.configuration.FactoryBuilder;
+import java.util.Arrays;
 
 public class IgniteClientNode {
 
@@ -19,6 +25,7 @@ public class IgniteClientNode {
     private IgniteCache<PropertyPK, Property> propertyCache;
     private IgniteCache<RatePlanPK, RatePlan> ratePlanIgniteCache;
     private IgniteCache<RatePlanAllocationPK, RatePlanAllocation> ratePlanAllocationIgniteCache;
+    private static final Logger LOGGER = LoggerFactory.getLogger(IgniteClientNode.class);
 
     public void startCache() {
 //        Ignition.setClientMode(true);
@@ -82,5 +89,24 @@ public class IgniteClientNode {
 
     public IgniteCache<RatePlanAllocationPK, RatePlanAllocation> getRatePlanAllocationIgniteCache() {
         return ratePlanAllocationIgniteCache;
+    }
+
+    public static void main(String[] args) {
+        Ignition.setClientMode(true);
+        org.apache.ignite.configuration.IgniteConfiguration igniteConfiguration = new org.apache.ignite.configuration.IgniteConfiguration();
+
+        DataStorageConfiguration storageCfg = new DataStorageConfiguration();
+        storageCfg.getDefaultDataRegionConfiguration().setPersistenceEnabled(true);
+        igniteConfiguration.setDataStorageConfiguration(storageCfg);
+
+        TcpDiscoverySpi tcpDiscoverySpi = new TcpDiscoverySpi();
+        TcpDiscoveryMulticastIpFinder ipFinder = new TcpDiscoveryMulticastIpFinder();
+        ipFinder.setAddresses(Arrays.asList("127.0.0.1:47500..47502"));
+        tcpDiscoverySpi.setIpFinder(ipFinder);
+        igniteConfiguration.setDiscoverySpi(tcpDiscoverySpi);
+
+        Ignite ignite = Ignition.start(igniteConfiguration);
+        ignite.active(true);
+        LOGGER.info("<<<<<<<<<<<<<<< Cache Started successfully");
     }
 }
